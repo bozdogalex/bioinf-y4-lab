ce3b18812492b95bc371ad9b41bd235a5e270824
Bogdan Şveţ <68112969+banmepls@users.noreply.github.com>
2025-11-03 13:32:04 +0000

    Lab 4 demo si exercitiu

---
+++ labs/04_phylogenetics/submissions/banmepls/banmepls_notes.md
@@ -0,0 +1,3 @@
+Am rulat demo-ul cu succes
+
+Am descarcat multi fasta, apoi aliniat in Clustal Omega, apoi rulat exercitiul
---
+++ labs/04_phylogenetics/submissions/banmepls/ex01_phylo_NJ.py
@@ -0,0 +1,38 @@
+"""
+Exercițiul 5 — Construirea unui arbore Neighbor-Joining
+
+Instrucțiuni (de urmat în laborator):
+1. Refolosiți secvențele din laboratoarele anterioare (FASTA din Lab 2 sau FASTQ→FASTA din Lab 3).
+2. Dacă aveți doar fișiere FASTA cu o singură secvență, combinați cel puțin 3 într-un fișier multi-FASTA:
+3. Salvați fișierul multi-FASTA în: data/sample/tp53_dna_multi.fasta
+4. Completați pașii de mai jos:
+   - încărcați multi-FASTA-ul,
+   - calculați matricea de distanțe,
+   - construiți arborele NJ,
+   - salvați rezultatul în format Newick (.nwk).
+"""
+
+from pathlib import Path
+from Bio import AlignIO, Phylo
+from Bio.Phylo.TreeConstruction import DistanceCalculator, DistanceTreeConstructor
+
+if __name__ == "__main__":
+    # TODO 1: Încărcați fișierul multi-FASTA propriu
+    fasta = Path("labs/04_phylogenetics/submissions/banmepls/sequence2.aln-fasta")
+
+    # Exemplu (decomentați după ce înlocuiți <handle>):
+    alignment = AlignIO.read(fasta, "fasta")
+
+    # TODO 2: Calculați matricea de distanțe
+    calculator = DistanceCalculator('identity')
+    dm = calculator.get_distance(alignment)
+
+    # TODO 3: Construiți arborele NJ
+    constructor = DistanceTreeConstructor()
+    tree = constructor.nj(dm)
+
+    # TODO 4: Salvați arborele în format Newick
+    Phylo.write(tree, "tree.nwk", "newick")
+
+    # TODO 5 Vizualizați arborele
+    Phylo.draw_ascii(tree)
