7632252463bbdee2275a607f18daaf420c17f477
Marius Jalbă <marius.jalba@student.upt.ro>
2025-11-12 00:38:43 +0000

    Lab 5 finished

---
+++ labs/05_clustering/MariusJalba_notes.md
@@ -0,0 +1,10 @@
+ce metodă ați considerat cea mai potrivită pentru datele analizate?
+
+Pentru datasetul Breast Cancer Wisconsin, K-means pare sa fie cea mai potrivita metoda, datele au fost standardizate si formeaza doua grupuri compacte: tumori maligne si beningne,
+ceea ce corespunde bine unui model cu K=2.
+DBSCAN identifica regiuni dense, distributia nu este foarte separabila, deci genereaza mult noise.
+Hierarchihal clustering ofera o perspectiva globala, dar greu de interpretat pentru seturi mari.
+
+**Cum se compară clustering-ul cu arborii filogenetici în descoperirea relațiilor biologice?**
+Clustering-ul si arborii filogenetici grupeaza ambele obiecte in functie de similaritate.
+Totusi clustering-ul este o abordare matematica si arata asemanari statistice, in timp ce arborii filogenetici au semnificatie bilogica, arborii descriu evolutia comuna si realtiile ancestrale intre specii
--- labs/05_clustering/submissions/MariusJalba/ex01_clustering.py
+++ labs/05_clustering/submissions/MariusJalba/ex01_clustering.py
@@ -15,16 +15,18 @@ Instrucțiuni:
    - kmeans_<handle>.png
    - dbscan_<handle>.png
 """
-
+from scipy.cluster.hierarchy import dendrogram, linkage
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from pathlib import Path
+from sklearn.cluster import KMeans, DBSCAN
+from sklearn.decomposition import PCA
 
 # Opțional: puteți importa deja funcțiile necesare
 # from scipy.cluster.hierarchy import dendrogram, linkage
 # from sklearn.cluster import KMeans, DBSCAN
-# from sklearn.decomposition import PCA
+# 
 
 if __name__ == "__main__":
     # TODO 1: Încărcați dataset-ul
@@ -44,25 +46,62 @@ if __name__ == "__main__":
     X_scaled = scaler.fit_transform(X)
 
     # Director pentru rezultate
-    output_dir = Path("labs/05_clustering/submissions/<handle>")
+    output_dir = Path("labs/05_clustering/submissions/MariusJalba")
     output_dir.mkdir(parents=True, exist_ok=True)
 
     # TODO 4: Hierarchical Clustering
-    linkage(X_scaled, method="average")
-    # - vizualizați cu dendrogram()
-    # - salvați imaginea ca hierarchical_<handle>.png
+    Z = linkage(X_scaled, method="average")
+    plt.figure(figsize=(50,30))
+    dendrogram(Z)
+    out_path = "labs/05_clustering/submissions/MariusJalba/hierarchical_MariusJalba.png"
+    plt.tight_layout()
+    plt.savefig(out_path, dpi=200)
+    plt.close()
 
     # TODO 5: K-means Clustering
     # - aplicați KMeans cu K=2
     # - adăugați etichetele în df["KMeans_Cluster"]
     # - reduceți dimensionalitatea cu PCA(n_components=2)
     # - vizualizați și salvați plotul kmeans_<handle>.png
+    kmeans = KMeans(n_clusters=2, init='k-means++', n_init='auto', max_iter=300, random_state=42)
+    kmeans_labels = kmeans.fit_predict(X_scaled)
+    df["KMeans_Cluster"] = kmeans_labels
+    pca = PCA(n_components=2, random_state=42)
+    X_pca = pca.fit_transform(X_scaled)
+    df["PC1"] = X_pca[:,0]
+    df["PC2"] = X_pca[:,1]
+    plt.figure(figsize=(10, 8))
+    plt.scatter(df["PC1"], df["PC2"], c=df["KMeans_Cluster"])
+    plt.title("KMeans (K=2) on WDBC – PCA(2D)")
+    plt.xlabel("PC1")
+    plt.ylabel("PC2")
+    plt.tight_layout()
+    out_scatter =  "labs/05_clustering/submissions/MariusJalba/kmeans_MariusJalba.png"
+    plt.savefig(out_scatter, dpi=200)
+    plt.close()
+
 
     # TODO 6: DBSCAN Clustering
     # - aplicați DBSCAN (ex: eps=1.5, min_samples=5)
     # - adăugați etichetele în df["DBSCAN_Cluster"]
     # - vizualizați și salvați plotul dbscan_<handle>.png
+    db = DBSCAN(eps=1.5, min_samples=5).fit(X_scaled)
+    labels = db.labels_
+    df["DBSCAN_Cluster"] = labels
+    coords = PCA(n_components=2, random_state=42).fit_transform(X_scaled) #aplicam PCA sa putem reprezenta toate dimensiunile in doar 2 axe
+    plt.scatter(coords[:,0], coords[:,1], c=df["DBSCAN_Cluster"], cmap="rainbow", s=10)
+    plt.title("DBSCAN Clustering (PCA 2D)")
+    plt.xlabel("PC1")
+    plt.ylabel("PC2")
+    plt.tight_layout()
+    plt.savefig("labs/05_clustering/submissions/MariusJalba/dbscan_MariusJalba.png")
+    plt.close()
 
     # TODO 7: Salvare rezultate
     # salvați un CSV cu coloanele ["Diagnosis", "KMeans_Cluster", "DBSCAN_Cluster"]
     # în clusters_<handle>.csv
+    path = "labs/05_clustering/submissions/MariusJalba/clusters_MariusJalba.csv"
+    df[["Diagnosis", "KMeans_Cluster","DBSCAN_Cluster"]].to_csv(path, index=False)
+
+    print("Toate fisierele de output au fost salvate in submissions/MariusJalba") 
+    
